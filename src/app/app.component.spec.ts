94ba45f9a51d4a5e6807309af464d1162d874dc7
WALKME\milton.m <milton.m@walkme.com>
2018-08-13 11:32:46 -0700

    Changes for TeamCity Build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,12 @@ describe('AppComponent', () => {
   it(`should have as title 'my-app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('Milton J Mendieta');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Milton J Mendieta');
   }));
 });
