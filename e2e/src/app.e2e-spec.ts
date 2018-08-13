66e32d401828e4c62687c4740bab17636e2b10ae
WALKME\milton.m <milton.m@walkme.com>
2018-08-13 13:37:08 -0700

    COmmited

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Milton J Mendieta');
+    expect(page.getParagraphText()).toEqual('Milton J Mendieta ');
   });
 });
